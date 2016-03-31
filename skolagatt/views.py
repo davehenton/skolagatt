da11bb7d9235e246ac7f39c46d2029d9ac2a1c7a
Björn Leví Gunarsson <bjornlev@gmail.com>
2016-03-31 12:46:02 +0000

    receiving redirect_to from innskraning

--- skolagatt/views.py
+++ skolagatt/views.py
@@ -6,23 +6,29 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
+import requests
+
+@csrf_exempt
 def index(request):
-    return render(request, 'index.html')
+	if(request.method == "POST"):
+		for k,v in request.POST.items():
+			print(k,v)
+	return render(request, 'index.html')
 
 @csrf_exempt
 @login_required
 def login(request):
-    if(request.method == "POST"):
-        user = authenticate(username=request.POST['user_ssn'], password=request.POST['user_name'])
-        if user is not None:
-            if user.is_active:
-                auth_login(request, user)
-                return redirect('index')
-    return redirect('denied')
+	if(request.method == "POST"):
+		user = authenticate(username=request.POST['user_ssn'], password=request.POST['user_name'])
+	if user is not None:
+		if user.is_active:
+			auth_login(request, user)
+			return redirect('index')
+	return redirect('denied')
 
 def logout(request):
-    auth_logout(request)
-    return redirect('index')
+	auth_logout(request)
+	return redirect('index')
 
 def denied(request):
-    return render(request, 'denied.html')
+	return render(request, 'denied.html')
