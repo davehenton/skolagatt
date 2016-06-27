001b3157aada19e6513fa1a1f2fabef07014fec2
Björn Leví <bjornlevi@gmail.com>
2016-06-27 10:16:52 +0000

    resolves #7, adding local login if settings.DEBUG = True

--- schools/views.py
+++ schools/views.py
@@ -156,6 +156,22 @@ class ManagerDetail(UserPassesTestMixin, DetailView):
     context['school'] = School.objects.get(pk=self.kwargs['school_id'])
     return context
 
+class ManagerOverview(UserPassesTestMixin, DetailView):
+  model = Manager
+
+  def test_func(self):
+    try:
+      if self.request.user.id == Manager.objects.get(pk=self.kwargs['pk']).user.id:
+        return True
+    except:
+      return False
+    return False
+
+  def get_context_data(self, **kwargs):
+    context = super(ManagerOverview, self).get_context_data(**kwargs)
+    context['school'] = School.objects.filter(managers=Manager.objects.filter(pk=self.kwargs['pk'])).first()
+    return context
+
 class ManagerCreate(UserPassesTestMixin, CreateView):
   model = Manager
   form_class = ManagerForm
@@ -258,6 +274,22 @@ class TeacherDetail(UserPassesTestMixin, DetailView):
     context = super(TeacherDetail, self).get_context_data(**kwargs)
     context['school'] = School.objects.get(pk=self.kwargs['school_id'])
     context['groups'] = StudentGroup.objects.filter(group_managers=Teacher.objects.filter(user=self.request.user))
+    return context
+
+class TeacherOverview(UserPassesTestMixin, DetailView):
+  model = Teacher
+
+  def test_func(self):
+    try:
+      if self.request.user.id == Teacher.objects.get(pk=self.kwargs['pk']).user.id:
+        return True
+    except:
+      return False
+    return False
+
+  def get_context_data(self, **kwargs):
+    context = super(TeacherOverview, self).get_context_data(**kwargs)
+    context['school'] = School.objects.filter(teachers=Teacher.objects.filter(pk=self.kwargs['pk'])).first()
     return context
 
 class TeacherCreate(UserPassesTestMixin, CreateView):
@@ -598,7 +630,7 @@ class StudentGroupUpdate(UserPassesTestMixin, UpdateView):
     print(school)
     context['school'] = School.objects.get(pk=self.kwargs['school_id'])
     context['students'] = Student.objects.filter(school=self.kwargs['school_id'])
-    
+
     return context
 
 class StudentGroupDelete(UserPassesTestMixin, DeleteView):
