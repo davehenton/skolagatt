83936cda299747b1593b5720afa7cd6dbadedfd5
7oi <johann@mms.is>
2016-12-05 16:30:30 +0000

    Possbly final flake8 compliance

--- supportandexception/urls.py
+++ supportandexception/urls.py
@@ -1,23 +1,23 @@
 from django.conf.urls import url
 from django.conf      import settings
-from .views           import *
+from .                import views
 
 
 app_name = 'supportandexception'
 urlpatterns = [
-	url(r'^'+settings.SUBDIRECTORY+'(?P<school_id>\d+)/$',
-		ExamSupport.as_view(),
-		name='examsupport'),
-	url(r'^'+settings.SUBDIRECTORY+'(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/$',
-		Detail.as_view(),
-		name='detail'),
-	url(r'^'+settings.SUBDIRECTORY+'(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/stuðningsúrræði/$',
-		SupportreResourceCreate.as_view(),
-		name='supportresource'),
-	url(r'^'+settings.SUBDIRECTORY+'(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/undanþágur/$',
-		ExceptionCreate.as_view(),
-		name='exception'),
-	url(r'^'+settings.SUBDIRECTORY+'api$',
-		StudentWithExceptViewSet.as_view({'get': 'list'}),
-		name='json'),
+    url(r'^' + settings.SUBDIRECTORY + '(?P<school_id>\d+)/$',
+        views.ExamSupport.as_view(),
+        name='examsupport'),
+    url(r'^' + settings.SUBDIRECTORY + '(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/$',
+        views.Detail.as_view(),
+        name='detail'),
+    url(r'^' + settings.SUBDIRECTORY + '(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/stuðningsúrræði/$',
+        views.SupportreResourceCreate.as_view(),
+        name='supportresource'),
+    url(r'^' + settings.SUBDIRECTORY + '(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/undanþágur/$',
+        views.ExceptionCreate.as_view(),
+        name='exception'),
+    url(r'^' + settings.SUBDIRECTORY + 'api$',
+        views.StudentWithExceptViewSet.as_view({'get': 'list'}),
+        name='json'),
 ]
