5c5a78a5b5e609520b37ff8cd9ad2179dc6f72ff
7oi <johann@mms.is>
2017-01-09 17:34:54 +0000

    Use DataTables in the right places, not all places

--- skolagatt/static/js/main.js
+++ skolagatt/static/js/main.js
@@ -4,3 +4,32 @@ $(function() {
         $( this ).html(marked( $( this ).html() ));
     });
 });
+var datafy_table = function(table) {
+	$(table).DataTable({
+		"bLengthChange": false,
+	    "pageLength": 20,
+		"language": {
+		    "sEmptyTable":     "Engin gögn eru í þessari töflu",
+		    "sInfo":           '',
+		    "sInfoEmpty":      "",
+		    "sInfoFiltered":   "(síað út frá _MAX_ færslum)",
+		    "sInfoPostFix":    "",
+		    "sInfoThousands":  ".",
+		    "sLengthMenu":     "Sýna _MENU_ færslur",
+		    "sLoadingRecords": "Hleð...",
+		    "sProcessing":     "Úrvinnsla...",
+		    "sSearch":         "Leita:",
+		    "sZeroRecords":    "Engar færslur fundust",
+		    "oPaginate": {
+		        "sFirst":    "Fyrsta",
+		        "sLast":     "Síðasta",
+		        "sNext":     "Næsta",
+		        "sPrevious": "Fyrri"
+		    },
+		    "oAria": {
+		        "sSortAscending":  ": virkja til að raða dálki í hækkandi röð",
+		        "sSortDescending": ": virkja til að raða dálki lækkandi í röð"
+		    }
+		}
+	});
+}
