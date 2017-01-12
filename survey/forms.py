c35c7bc08903f7985a09f3e43cc82464f1cb0de2
Henry Baldursson <henry.baldursson@gmail.com>
2017-01-12 13:30:10 +0000

    HTML/css fixes

--- survey/forms.py
+++ survey/forms.py
@@ -53,13 +53,13 @@ class SurveyForm(forms.ModelForm):
             years = range(2016, timezone.now().year + 10),
             months=MONTHS,
             attrs={
-                'class': 'form-control disabled',
+                'class': 'form-control',
                 'style': 'width: 26%; display: inline-block;',
                 'disabled': True,
             })
+        self.fields['support_and_exception_allowed'].widget.attrs['class'] = ''
         fields = [
             'identifier', 'title', 'survey_type', 'student_year', 'description',
-            'support_and_exception_allowed',
         ]
         add_field_classes(self, fields)
 
