c94fa80a64f7c99890b93373b41bf8ed6a8c8917
Henry Baldursson <henry.baldursson@gmail.com>
2017-01-24 09:17:33 +0000

    different approach

--- supportandexception/urls.py
+++ supportandexception/urls.py
@@ -2,27 +2,21 @@ from django.conf.urls import url
 from .                import views
 
 
-app_name = 'supportandexception'
+app_name = "supportandexception"
 urlpatterns = [
-    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/stuðningsúrræði/$',
+    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/$',
         views.ExamSupport.as_view(),
-        name="newsupporturl"),
-    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/undanþágur/$',
-        views.ExamSupport.as_view(),
-        name="newexceptionurl"),
-    url(r'^(?P<school_id>\d+)/$',
-        views.ExamSupport.as_view(),
-        name='examsupport'),
-    url(r'^(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/$',
+        name="examsupport"),
+    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/nemandi/(?P<pk>[0-9]+)/$',
         views.Detail.as_view(),
-        name='detail'),
-    url(r'^(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/stuðningsúrræði/$',
+        name="detail"),
+    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/nemandi/(?P<pk>[0-9]+)/stuðningsúrræði/$',
         views.SupportResourceCreate.as_view(),
-        name='supportresource'),
-    url(r'^(?P<school_id>\d+)/nemandi/(?P<pk>[0-9]+)/undanþágur/$',
+        name="supportresource"),
+    url(r'^skoli/(?P<school_id>\d+)/prof/(?P<groupsurvey_id>\d+)/nemandi/(?P<pk>[0-9]+)/undanþágur/$',
         views.ExceptionCreate.as_view(),
-        name='exception'),
+        name="exception"),
     url(r'^api$',
         views.StudentWithExceptViewSet.as_view({'get': 'list'}),
-        name='json'),
+        name="json"),
 ]
