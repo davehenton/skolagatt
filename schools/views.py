447082032aa90315cf09d5fb1fdf08141cc44fbe
Henry Baldursson <henry.baldursson@gmail.com>
2017-02-17 12:43:12 +0000

    Bætti við aðgerð til að skoða allt landið í einu kasti

--- schools/views.py
+++ schools/views.py
@@ -2125,3 +2125,187 @@ def lesfimi_excel_for_principals(request, pk):
     return response
 
 
+def lesfimi_excel_entire_country_stats():
+    ref_values = {
+        1:  (20, 55, 75),
+        2:  (40, 85, 100),
+        3:  (55, 100, 120),
+        4:  (80, 120, 145),
+        5:  (90, 140, 160),
+        6:  (105, 155, 175),
+        7:  (120, 165, 190),
+        8:  (130, 180, 210),
+        9:  (140, 180, 210),
+        10: (145, 180, 210),
+    }
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=Lesfimi - Allt Landið.xlsx'
+
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    wb.remove_sheet(ws)     
+
+    tests = (
+        ('b{}_LF_jan17', 'Janúar 2017'),
+        ('{}b_LF_sept', 'September 2016'),
+    )
+    for test in tests:
+        identifier = test[0]
+        title = test[1]
+        ws = wb.create_sheet(title = title)
+        ws['A1'] = 'Árgangur'
+        ws['B1'] = 'Fjöldi nemenda'
+        ws['C1'] = 'Fjöldi sem þreytti próf'
+        ws['D1'] = 'Hlutfall sem þreytti próf'
+        ws['E1'] = 'Hlutfall sem nær 90% viðmiðum'
+        ws['F1'] = 'Hlutfall sem nær 50% viðmiðum'
+        ws['G1'] = 'Hlutfall sem nær 25% viðmiðum'
+        index = 2
+        errors = []
+        for year in range(1,11):
+            ws['A' + str(index)] = year
+            survey = Survey.objects.filter(identifier = identifier.format(year)).first()
+            survey_type = SurveyType.objects.filter(survey=survey.id).values('id')
+            dic = survey_type[0]
+            survey_type = dic['id']
+            transformation = SurveyTransformation.objects.filter(survey=survey)
+            studentgroups = StudentGroup.objects.filter(student_year = year).all()
+            this_year_result = {
+                'students': 0,
+                'students_who_took_test': 0,
+                'students_over_25pct': 0,
+                'students_over_50pct': 0,
+                'students_over_90pct': 0,
+            }
+            for studentgroup in studentgroups:
+                this_year_result['students'] += studentgroup.students.all().count()
+                groupsurveys = GroupSurvey.objects.filter(studentgroup = studentgroup, survey = survey)
+                if groupsurveys.all().count() > 1:
+                    errors.append('sama próf skráð {} sinnum fyrir {}'.format(groupsurveys.all().count(), studentgroup.name))
+                for groupsurvey in groupsurveys.all():
+                    for student in studentgroup.students.all():
+                        surveyresults = SurveyResult.objects.filter(survey = groupsurvey, student = student)
+                        if surveyresults.all().count() > 1:
+                            errors.append('{} niðurstöður í sama prófi skráðar fyrir nemanda {} í bekk {} í {}'.format(
+                                surveyresults.all().count(),
+                                student.ssn,
+                                studentgroup.name,
+                                studentgroup.school.name,
+                            ))
+                        for surveyresult in surveyresults.all():
+                            r = literal_eval(surveyresult.results)  # get student results
+                            try:
+                                survey_student_result = common_util.calc_survey_results(
+                                    survey_identifier = identifier,
+                                    click_values = literal_eval(r['click_values']),
+                                    input_values = r['input_values'],
+                                    student = student,
+                                    survey_type = survey_type,
+                                    transformation = transformation,
+                                )
+                                #import pdb; pdb.set_trace()
+                                if not survey_student_result[0] == '':
+                                    this_year_result['students_who_took_test'] += 1
+                                    if int(survey_student_result[0]) >= ref_values[year][2]:
+                                        this_year_result['students_over_25pct'] += 1
+                                    if int(survey_student_result[0]) >= ref_values[year][1]:
+                                        this_year_result['students_over_50pct'] += 1
+                                    if int(survey_student_result[0]) >= ref_values[year][0]:
+                                        this_year_result['students_over_90pct'] += 1
+                            except:
+                                pass
+
+            ws['B' + str(index)] = this_year_result['students']
+            ws['C' + str(index)] = this_year_result['students_who_took_test']
+            if this_year_result['students'] > 0 and this_year_result['students_who_took_test'] > 0:
+                ws['D' + str(index)] = (this_year_result['students_who_took_test'] / this_year_result['students']) * 100
+                pct_over_90pct = (this_year_result['students_over_90pct'] / this_year_result['students_who_took_test']) * 100
+                ws['E' + str(index)] = pct_over_90pct
+
+                pct_over_50pct = (this_year_result['students_over_50pct'] / this_year_result['students_who_took_test']) * 100
+                ws['F' + str(index)] = pct_over_50pct
+
+                pct_over_25pct = (this_year_result['students_over_25pct'] / this_year_result['students_who_took_test']) * 100
+                ws['G' + str(index)] = pct_over_25pct
+
+            else:
+                ws['D' + str(index)] = 0
+                ws['E' + str(index)] = 0
+                ws['F' + str(index)] = 0
+                ws['G' + str(index)] = 0
+
+            index += 1
+        dims = {}
+        for row in ws.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column] = max((dims.get(cell.column, 0), len(str(cell.value))))
+        for col, value in dims.items():
+            ws.column_dimensions[col].width = int(value) + 2
+
+        chart = AreaChart()
+        chart.title = "Lesfimi í {} - Allt landið".format(title)
+        chart.style = 10
+        chart.width = 40
+        chart.height = 20
+
+        chart.layout = Layout(
+                ManualLayout(
+                    xMode="edge",
+                    yMode="edge",
+                )
+            )
+
+
+        chart.x_axis.title = 'Árgangur'
+        chart.y_axis.title = 'Prósent'
+
+        chart.y_axis.scaling.min = 0
+        chart.y_axis.scaling.max = 100
+
+        cats = Reference(ws, min_col=1, min_row=2, max_row=index - 1)
+        data = Reference(ws, min_col=5, min_row=1, max_col=7, max_row=index)
+        chart.add_data(data, titles_from_data=True)
+        chart.set_categories(cats)
+
+
+        bchart = BarChart()
+        bchart.title = "Hlutfall nemenda sem þreyttu próf"
+        bchart.style = 10
+        bchart.width = 20
+        bchart.height = 10
+
+        bchart.x_axis.title = 'Árgangur'
+        bchart.y_axis.title = 'Prósent'
+
+        bchart.y_axis.scaling.min = 0
+        bchart.y_axis.scaling.max = 100
+
+        bdata = Reference(ws, min_col=4, max_col=4, min_row=2, max_row=index)
+        bchart.add_data(bdata)
+        bchart.legend = None
+        bchart.set_categories(cats)
+        ws.add_chart(bchart, "I1")
+
+        if index > 20:
+            ws.add_chart(chart, "A" + str(index + 2))
+        else:
+            ws.add_chart(chart, "A22")
+
+        if errors:
+            ws = wb.create_sheet(title = 'Villur')
+            index = 1
+            for error in errors:
+                ws['A' + str(index)] = "ATH: " + error
+                ws['A' + str(index)].fill = PatternFill(start_color='ff0000', end_color='ff0000', fill_type='solid')
+                ws.merge_cells('A' + str(index) + ':F' + str(index))
+                index += 1
+
+
+    wb.save(filename='/tmp/test.xlsx')
+    #wb.save(response)
+
+    return response
+
+
