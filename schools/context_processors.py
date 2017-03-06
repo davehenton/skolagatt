f09dd12c0b628e2e74f9988dc4e37b5ab7fbd62b
Henry Baldursson <henry.baldursson@gmail.com>
2017-03-06 15:53:41 +0000

    removing pdb line

--- schools/context_processors.py
+++ schools/context_processors.py
@@ -2,7 +2,6 @@ import common.util as cm_util
 
 
 def user_school_permissions(request):
-    #import pdb; pdb.set_trace()
     kwargs = request.resolver_match.kwargs if request.resolver_match else {}
     response = {
         'is_school_manager': cm_util.is_school_manager(request, kwargs),
