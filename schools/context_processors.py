aa887e0b194faf5a5d45dff5ee2f87737e10fbba
Henry Baldursson <henry.baldursson@gmail.com>
2017-03-06 15:17:27 +0000

    Making things more robust

--- schools/context_processors.py
+++ schools/context_processors.py
@@ -2,9 +2,11 @@ import common.util as cm_util
 
 
 def user_school_permissions(request):
+    #import pdb; pdb.set_trace()
+    kwargs = request.resolver_match.kwargs if request.resolver_match else {}
     response = {
-        'is_school_manager': cm_util.is_school_manager(request, request.resolver_match.kwargs),
-        'is_school_teacher': cm_util.is_school_teacher(request, request.resolver_match.kwargs),
-        'is_group_manager' : cm_util.is_group_manager(request, request.resolver_match.kwargs),
+        'is_school_manager': cm_util.is_school_manager(request, kwargs),
+        'is_school_teacher': cm_util.is_school_teacher(request, kwargs),
+        'is_group_manager' : cm_util.is_group_manager(request, kwargs),
     }
     return response
