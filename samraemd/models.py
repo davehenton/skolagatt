e121aa7be02d738077fb33d3562a969da996f0e5
Henry Baldursson <henry.baldursson@gmail.com>
2017-03-27 09:54:09 +0000

    Refactor, reusable inheritance

--- samraemd/models.py
+++ samraemd/models.py
@@ -9,8 +9,38 @@ from jsonfield    import JSONField
 from common.models import Student
 
 
-class SamraemdMathResult(models.Model):
+_YEAR_IN_SCHOOL_CHOICES = (
+        ('1', '1. bekkur'),
+        ('2', '2. bekkur'),
+        ('3', '3. bekkur'),
+        ('4', '4. bekkur'),
+        ('5', '5. bekkur'),
+        ('6', '6. bekkur'),
+        ('7', '7. bekkur'),
+        ('8', '8. bekkur'),
+        ('9', '9. bekkur'),
+        ('10', '10. bekkur'),
+    )
+
+class _SamraemdBaseModel(models.Model):
     student = models.ForeignKey(Student)
+    exam_date = models.DateField()
+    
+    class Meta:
+        abstract = True
+
+
+class _SamraemdStudentYear(models.Model):
+    student_year = models.CharField(
+        max_length=2,
+        choices=_YEAR_IN_SCHOOL_CHOICES,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class SamraemdMathResult(_SamraemdBaseModel, _SamraemdStudentYear):
     # data fields
     # Samræmd einkunn
     ra_se = models.CharField(max_length = 4)
@@ -33,27 +63,9 @@ class SamraemdMathResult(models.Model):
     fm_txt = models.CharField(max_length = 256, blank=True)
     # exam fields
     exam_code = models.CharField(max_length = 128)
-    exam_date = models.DateField()
-    YEAR_IN_SCHOOL_CHOICES = (
-        ('1', '1. bekkur'),
-        ('2', '2. bekkur'),
-        ('3', '3. bekkur'),
-        ('4', '4. bekkur'),
-        ('5', '5. bekkur'),
-        ('6', '6. bekkur'),
-        ('7', '7. bekkur'),
-        ('8', '8. bekkur'),
-        ('9', '9. bekkur'),
-        ('10', '10. bekkur'),
-    )
-    student_year = models.CharField(
-        max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
-    )
 
 
-class SamraemdISLResult(models.Model):
-    student = models.ForeignKey(Student)
+class _SamraemdLANGResult(_SamraemdBaseModel, _SamraemdStudentYear):
     # data fields
     # Samræmd einkunn
     le_se = models.CharField(max_length=4)
@@ -73,53 +85,35 @@ class SamraemdISLResult(models.Model):
     # Framfaraeinkunn
     fm_fl  = models.CharField(max_length = 32, blank=True)
     fm_txt = models.CharField(max_length = 256, blank=True)
-    # exam fields
-    exam_code = models.CharField(max_length = 256)
-    exam_date = models.DateField()
-    YEAR_IN_SCHOOL_CHOICES = (
-        ('1', '1. bekkur'),
-        ('2', '2. bekkur'),
-        ('3', '3. bekkur'),
-        ('4', '4. bekkur'),
-        ('5', '5. bekkur'),
-        ('6', '6. bekkur'),
-        ('7', '7. bekkur'),
-        ('8', '8. bekkur'),
-        ('9', '9. bekkur'),
-        ('10', '10. bekkur'),
-    )
-    student_year = models.CharField(
-        max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
-    )
+
+    exam_code = models.CharField(max_length = 128)
+
+    class Meta:
+        abstract = True
 
 
-class SamraemdResult(models.Model):
-    student = models.ForeignKey(Student)
+class SamraemdISLResult(_SamraemdLANGResult):
+    pass
+
+
+class SamraemdENSResult(_SamraemdLANGResult):
+    pass
+
+
+class SamraemdResult(_SamraemdBaseModel):
     CODES   = (
         (1, 'Íslenska'),
         (2, 'Enska'),
         (3, 'Stærðfræði'),
     )
+
     exam_code = models.CharField(max_length=2, choices=CODES, null=True)
     exam_name = models.CharField(max_length=125, null=True)
-    exam_date = models.DateField(default=timezone.now)
-    YEAR_IN_SCHOOL_CHOICES = (
-        ('1', '1. bekkur'),
-        ('2', '2. bekkur'),
-        ('3', '3. bekkur'),
-        ('4', '4. bekkur'),
-        ('5', '5. bekkur'),
-        ('6', '6. bekkur'),
-        ('7', '7. bekkur'),
-        ('8', '8. bekkur'),
-        ('9', '9. bekkur'),
-        ('10', '10. bekkur'),
-    )
     student_year = models.CharField(
         max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
+        choices=_YEAR_IN_SCHOOL_CHOICES,
         null=True
     )
+
     result_data   = JSONField()
     result_length = models.CharField(max_length=4)
